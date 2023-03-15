cd586c4fbacf149586ee835ae1712fbba69eb5d0
Scott Rod <djmtype@gmail.com>
2023-03-15 13:05:46 -0400

    update icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -43,19 +43,25 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: 'icon-192.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: "any"
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'icon-192-maskable.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: "maskable"
+          },
+          {
+            src: 'icon-512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: "any"
           },
           {
-            src: 'pwa-maskable.png',
+            src: 'icon-512-maskable.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: "maskable"
